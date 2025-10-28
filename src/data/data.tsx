70ad18ed06bbcbacb9124cedacedf16c28ed5b59
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-10-28 16:47:57 +0100

    img folders

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -9,7 +9,7 @@ const Zizis: SuperZizi[] = [
     "accessoire": "Slips en titane",
     "faiblesse": "Savon trop glissant",
     "devise": "Toujours droit devant !",
-    "image": "./src/assets/images/super-zizi.png"
+    "image": "./public/img/super-zizi.png"
   },
   {
     "id": 2,
@@ -19,7 +19,7 @@ const Zizis: SuperZizi[] = [
     "accessoire": "Casquette rétro",
     "faiblesse": "Frigidaire géant",
     "devise": "Plus fort, plus long !",
-    "image": "./src/assets/images/zizi-elastic.png"
+    "image": "./public/img/zizi-elastic.png"
   },
   {
     "id": 3,
@@ -29,7 +29,7 @@ const Zizis: SuperZizi[] = [
     "accessoire": "Sceptre étoilé",
     "faiblesse": "Fou rire",
     "devise": "Time-out pour tous !",
-    "image": "./src/assets/images/miss-clito.jpeg"
+    "image": "./public/img/miss-clito.jpeg"
   },
   {
     "id": 4,
@@ -39,7 +39,7 @@ const Zizis: SuperZizi[] = [
     "accessoire": "Tableau magique",
     "faiblesse": "Trop de pizzas",
     "devise": "Le cerveau avant tout !",
-    "image": "./src/assets/images/professeur-mst.png"
+    "image": "./public/img/professeur-mst.png"
   },
   {
     "id": 5,
@@ -49,7 +49,7 @@ const Zizis: SuperZizi[] = [
     "accessoire": "Ceinture-piton",
     "faiblesse": "Allergie au nylon",
     "devise": "Protection maximum !",
-    "image": "./src/assets/images/capitaine-braguette.jpg"
+    "image": "./public/img/capitaine-braguette.jpg"
   },
   {
     "id": 6,
@@ -59,7 +59,7 @@ const Zizis: SuperZizi[] = [
     "accessoire": "Cape multifonction",
     "faiblesse": "Lavage à haute température",
     "devise": "Personne ne m'attrapera !",
-    "image": "./src/assets/images/mystere-slipman.jpg"
+    "image": "./public/img/mystere-slipman.jpg"
   },
   {
     "id": 7,
@@ -69,7 +69,7 @@ const Zizis: SuperZizi[] = [
     "accessoire": "De l'alcool",
     "faiblesse": "Les sous vetements",
     "devise": "J'apporte toujours “l'ambiance”.",
-    "image": "./src/assets/images/tonton-fesse-tival.jpg"
+    "image": "./public/img/tonton-fesse-tival.jpg"
   }
 ];
 
