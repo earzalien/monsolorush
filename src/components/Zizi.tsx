6c0f814190bff35afc44cea62ccf06d42cb9e059
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-10-29 10:45:37 +0100

    button cursor

--- src/components/Zizi.tsx
+++ src/components/Zizi.tsx
@@ -9,14 +9,14 @@ function Zizi({ zizi }: ZiziProps) {
     <article className="zizi-card">
         <img onClick={() => setOpenCard(o => !o)}
       aria-expanded={openCard} src={zizi.image} alt={`Portrait de ${zizi.nom}`} width="150px" height="150px" className="zizi-img" />
-        <h3>{zizi.nom}</h3><button type="button" onClick={() => setCounterLike((counterLike) => counterLike + 1)}>❤️ : {counterLike}</button>
+        <h3>{zizi.nom}</h3><button type="button" className="button" onClick={() => setCounterLike((counterLike) => counterLike + 1)}>❤️ : {counterLike}</button>
       {openCard && (
         <section>
           <p><strong>Costume :</strong> {zizi.costume}</p>
           <p><strong>Pouvoir :</strong> {zizi.pouvoir}</p>
           <p><strong>Accessoire :</strong> {zizi.accessoire}</p>
           <p><strong>Faiblesse :</strong> {zizi.faiblesse}</p>
-          <blockquote className="zizi-devise">"{zizi.devise}"</blockquote>
+          <p><strong>Devise :</strong><blockquote className="zizi-devise"> "{zizi.devise}"</blockquote></p>
         </section>
         )}
     </article>
